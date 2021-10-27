156c5758317d4f71ae672c9cacfda22fc80835e3
hamadsr <hamad.sarrar.eam.js@tuwaiq.edu.sa>
2021-10-28 00:02:29 +0300

    project1 update

--- project-1/JS/index.js
+++ project-1/JS/index.js
@@ -8,7 +8,7 @@ const Sectionitem = {
                 name: "Rexona for Men",
                 disc: "Men can sweat up to 50% more than women. That's why Rexona created For Men.",
                 image: "img/product1m.webp",
-                price: 25 
+                price: 25
             },
             {
                 name: "Adidas Dare",
@@ -26,31 +26,31 @@ const Sectionitem = {
                 name: "Shaver Pack",
                 disc: "All devices have stainless steel baldes for greater coverage and a faster shave.",
                 image: "img/product4m.webp",
-                price: 90 
+                price: 90
             },
             {
                 name: "Restoria Men",
                 disc: "Say goodbye to grey hair forever and start looking as young as you feel today!",
                 image: "img/product5m.webp",
-                price: 95 
+                price: 95
             },
             {
                 name: "Nivea Face Cream 50ml",
                 disc: "Skin feels instantly moisturised, without feeling sticky",
                 image: "img/product7m.webp",
-                price: 15 
+                price: 15
             },
             {
                 name: "Men's Skincare Facial Wash",
                 disc: "natural anti-ageing ingredients Hyaluronic Acid and Vitamin E",
                 image: "img/product8m.webp",
-                price: 35 
+                price: 35
             },
             {
                 name: "Evolis Shampoo for Men 300ml",
                 disc: "Delivers instant volume with unique catch and release polymer",
                 image: "img/product9m.webp",
-                price: 75 
+                price: 75
             },
 
 
@@ -61,36 +61,29 @@ const Sectionitem = {
         title: "for Women",
         content: "Women's product ",
         image: "img/women-logo-1.jpg",
-        items: [
-            {
+        items: [{
                 name: "Ego QV Cream 1kg",
                 disc: "Particularly suitable for infants and the elderly. QV Cream is free from lanolin.",
                 image: "img/product4w.webp",
-                price: 115 
+                price: 115
             },
             {
                 name: "Skin Control Pimple Patch AM + PM X 36",
                 disc: "Cleanses pimples, Protects from dirt, Helps absorb whiteheads",
                 image: "img/product5w.webp",
-                price: 135 
+                price: 135
             },
             {
                 name: "Vita gummies",
                 disc: "Nature's Way Adult Vita Gummies Women's Multivitamin Pastilles X100",
                 image: "img/product2w.webp",
-                price: 100 
+                price: 100
             },
             {
                 name: "Nature's Way Kids",
                 disc: "Nature's Way Kids Smart Vita Gummies Multi-Vitamin for Fussy Eaters X60",
                 image: "img/product3w.webp",
-                price: 95 
-            },
-            {
-                name: "Nivea",
-                disc: "Nivea Anti-Perspirant Deodorant Roll On For Women Sensitive Protect 50ml",
-                image: "img/product6m.webp",
-                price: 15 
+                price: 95
             },
             {
                 name: "Nivea",
@@ -98,7 +91,13 @@ const Sectionitem = {
                 image: "img/product6m.webp",
                 price: 15
             },
-    ],
+            {
+                name: "Nivea",
+                disc: "Nivea Anti-Perspirant Deodorant Roll On For Women Sensitive Protect 50ml",
+                image: "img/product6m.webp",
+                price: 15
+            },
+        ],
     },
     Kids: {
         title: "For Kids",
@@ -115,7 +114,7 @@ const Sectionitem = {
 
 //use for in to show the sections in homepage
 for (const property in Sectionitem) {
- $(".sections").append(`
+    $(".sections").append(`
     <div class="col-12 col-md-4" >
     <div class="card">
         <img src="${Sectionitem[property]['image']}" class="card-img-top" alt="...">
@@ -134,8 +133,8 @@ for (const property in Sectionitem) {
         $(".HomePage").css("display", "none");
         $(".basket").css("display", "none");
 
-        Sectionitem[property]['items'].forEach(elem =>{
-       
+        Sectionitem[property]['items'].forEach(elem => {
+
             $(".Section-item").append(`
             <div class="col-12 col-md-3" id="${elem.name}" >
             <div class="card">
@@ -144,25 +143,25 @@ for (const property in Sectionitem) {
                     <h5 class="card-title">${elem.name} </h5>
                     <p class="card-text">${elem.disc} </p>
                     <h6>${elem.price} SAR</h6>
-                    <a onclick= "addToCart(${elem.price})" href="#"  class="btn btn-primary" style="background-color: #14279B;"><i
+                    <a onclick= "addToCart(${elem.price})" id="addd"  href="#"  class="btn btn-primary" style="background-color: #14279B;"><i
                             class="bi bi-cart4"></i></a>
                 </div>
             </div>
         </div>`)
         });
-        
+
     });
-    
 
-   
-    };
 
-    //local stoarge for object
-    let myobj=JSON.stringify(Sectionitem);
+
+};
+
+//local stoarge for object
+let myobj = JSON.stringify(Sectionitem);
 
 localStorage.setItem("Sectionitem", myobj);
 
-let myobjdes= JSON.parse(localStorage.getItem("Sectionitem"));
+let myobjdes = JSON.parse(localStorage.getItem("Sectionitem"));
 
 
 
@@ -185,52 +184,124 @@ $("#Create").click(function () {
 
 //sign up
 
-$("#Create-Account").click(function(){
+$("#Create-Account").click(function () {
 
     let newuser = $("#username").val();
     let newPass = $("#Pass").val();
-    let newEmail= $("#Email").val();
+    let newEmail = $("#Email").val();
 
-    if(newuser!== "" && newPass!== "" && newEmail!== "" ){
+    if (newuser !== "" && newPass !== "" && newEmail !== "") {
 
-        const users= {
-            name:newuser,
+        const users = {
+            name: newuser,
             password: newPass,
             email: newEmail,
         };
         const listofUsers = JSON.parse(localStorage.getItem('Data'));
 
-        if(listofUsers!== null){
+        if (listofUsers !== null) {
             listofUsers.push(users);
             localStorage.setItem('Data', JSON.stringify(listofUsers));
 
-        }
-        else{
-            let newcustomer=[];
+        } else {
+            let newcustomer = [];
             newcustomer.push(users);
             localStorage.setItem('Data', JSON.stringify(newcustomer));
         }
-        
-        $('.sign-up').css('display','none');
+
+        $('.sign-up').css('display', 'none');
         $('.sign-in').css('display', 'block');
-    }
-    else{
-        $('.sign-up').css('display','block');
+    } else {
+        $('.sign-up').css('display', 'block');
     }
 
 });
+
+$('#btnSignIn').click(function () {
+    // let newuser = $('#user1').val(); 
+    // let newPass = $('#passw').val(); 
+
+    // const listofUsers = JSON.parse(localStorage.getItem('Data')); 
+
+    // if (newuser !== "" && newPass !== "") {
+    //     listofUsers.forEach(function (user) { 
+    //         if (newuser === user.name && newPass === user.password) {
+    //             alert(`Hello ${user.name}`);
+    //             // $('.sign-in').css('display', 'none'); 
+    //             // $('.HomePage').css('display', 'block');
+    //             //chicking
+    //             // sessionStorage.setItem('UserName', newuser); 
+    //             // const userLogin = sessionStorage.getItem('UserName'); 
+    //             // if (userLogin !== null) { 
+    //             //     // $('.HomePage').css('display', 'block'); 
+    //             //     // $('.sign-in').css('display', 'none'); 
+    //             // } 
+    //         } else { 
+    //             alert("WR");
+    //             $('.HomePage').css('display', 'none'); 
+    //             $('.sign-in').css('display', 'block');
+    //         } 
+    //     }); 
+    // }else{
+    //     alert("Enter Data");
+    // }
+
+    let inputUser = $('#user1').val();
+    let inputPassword = $('#passw').val();
+
+    const usersList = JSON.parse(localStorage.getItem('Data'));
+
+    if (inputUser !== "" && inputPassword !== "") {
+        usersList.forEach(function (user) {
+            if (inputUser === user.name && inputPassword === user.password) {
+                $('.sign-in').css('display', 'none');
+                $('.HomePage').css('display', 'block');
+            } else {
+
+            }
+        });
+    } else {
+        alert('Fill Data');
+    }
+
+});
+
 // cart 
-let arr =[];
-function addToCart(id){
+let arr = [];
+
+function addToCart(id) {
     arr.push(id);
 
 
-    localStorage.setItem('dataCart',JSON.stringify(arr) );
-    let num= JSON.parse(localStorage.getItem('dataCart') );
+    localStorage.setItem('dataCart', JSON.stringify(arr));
+    let num = JSON.parse(localStorage.getItem('dataCart'));
     $('#cartt').html(num.length);
-    
+
 };
 
 
+$("#bass").click(function () {
+    $(".HomePage").css("display", "none");
+    $(".Sectionpage").css("display", "none");
+    $(".sign-in").css("display", "none");
+    $(".sign-up").css("display", "none");
+    $(".basket").css("display", "bluck");
+});
 
-
+// take the items to cart
+const cartUser = JSON.parse(localStorage.getItem('cartUser'));
+favoriteUserList.forEach(element => {
+    for (const key in element) {
+        for (const item in element[key]['Favorites']) {
+            $('.row-cols-1').append(
+                `<div class="col-12 col-md-3">
+                    <div class="card">
+                        <img src="${element[key]['Favorites'][item]['image']}" class="card-img-top" alt="..." height:"170px">
+                        <div class="card-body">
+                            <h5 class="card-title">${element[key]['Favorites'][item]['name']}</h5>
+                        </div>
+                    </div>
+                    </div>`);
+        }
+    }
+});
